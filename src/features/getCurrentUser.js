f28e68ffd27655f274160c36a88c657ff37f72de
sumitjangir683 <138011255+sumitjangir683@users.noreply.github.com>
2024-07-09 01:56:35 +0530

    Update getCurrentUser.js

--- src/features/getCurrentUser.js
+++ src/features/getCurrentUser.js
@@ -2,10 +2,10 @@ import React from 'react'
 import axios from 'axios';
 const getCurrentUser = async () => {
     try {
-      const response = await axios.post("https://blog-nest-backend.vercel.app/api/v1/users/current-user",{},  {
-  withCredentials: true, // Include credentials (cookies) in the request
-  headers: {
-    'Content-Type': 'application/json'
+      const response = await axios.post("https://blog-nest-backend.vercel.app/api/v1/users/current-user",{},{
+      withCredentials: true, // Include credentials (cookies) in the request
+       headers: {
+      'Content-Type': 'application/json'
   })
      console.log(response);
       if (response) {
